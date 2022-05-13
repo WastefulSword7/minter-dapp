781ce83b8636354ac608256e68ade79f84a98e03
WastefulSword7 <carter.gerhardt1206@gmail.com>
2022-05-13 15:23:18 -0500

    desc

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,10 +1,10 @@
 const contractAddress = "0x965254A4CD37F4ecc7135afaa4e5A4351ABA5A74"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the DadLife Website";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
+const welcome_p = "DadLife is a collection of 2500 unique dads. Your dad will help support single parent families - a portion of profits will be donated to Single Parent Advocate. Owning a dad will grant whitelist access to future NFT collections in partnership with DadLife. All our dad's lives are cool, but some are cooler.";
+const h1_presale_coming_soon = "Dads Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
@@ -15,10 +15,10 @@ const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! �
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Mint Your Dad";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_public_mint = "Mint Your Dad";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
